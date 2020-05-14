539b091c98b0e2fed7a30b052e8f5f2602e83723
Debabrata Nayak <debnayak@publicisgroupe.net>
2020-05-14 12:07:17 +0530

    Project Layout and Header added

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './layout.module.css';
+const siteTitle = "Hacker News";
+export default ({ children }) => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta
+                    name="description"
+                    content="A clone of Hacker News"
+                />
+                <meta name="og:title" content={siteTitle} />
+                <title>{siteTitle}</title>
+            </Head>
+            <header className={styles.header}>
+                <Link href="/"><a><img className={styles.headerImage} src="/logo.gif" alt="Logo" height="13" width="13" /></a></Link>
+                <Link href="/top"><a className={styles.active}>top</a></Link> | <Link href="/new"><a>new</a></Link>
+            </header>
+            <main className={styles.main}>{ children }</main>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Layout from "../components/Layout";
+
 export default () => {
     return (
-        <div>
+        <Layout>
             Welcome
-        </div>
+        </Layout>
     );
 }
